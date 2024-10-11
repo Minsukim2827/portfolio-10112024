2cf85649be37dee2c45c53e4c26991c590b2edf2
Minsu Kim <m.kim006064@gmail.com>
2024-10-11 21:36:44 +1300

    added some mobile responsiveness to intro

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,20 +1,19 @@
-// ./Intro.jsx
 import { Link } from 'react-router-dom';
+
 import SocialIcon from './_components/socialIcons';
 import socialLinks from './_components/socialLinks';
-import AnimatedText from './_components/AnimatedText'; // Import the AnimatedText component
-
+import AnimatedText from './_components/AnimatedText';
 export default function Intro() {
   return (
     <div
       className="min-h-[calc(95vh-var(--navbar-height))] text-foreground flex flex-col md:flex-row items-center justify-center p-4 md:p-6 gap-6 md:gap-8"
     >
       {/* Text Content */}
-      <div className="md:w-1/2 space-y-5 text-center md:text-left mb-3 md:mb-0">
-        <h1 className="text-6xl font-bold text-white">
+      <div className=" md:w-1/2 space-y-5 text-center md:text-left mb-3 md:mb-0">
+        <h1 className="text-2xl md:text-6xl font-bold text-white">
           Kia ora, I&apos;m <span className="text-red-500">Minsu</span>
         </h1>
-        <h2 className="text-4xl font-semibold text-white">
+        <h2 className="text-2xl md:text-4xl font-semibold text-white">
           I&apos;m a <AnimatedText />
         </h2>
         <p className="text-2xl text-white">Final year Software Development Major at AUT</p>
